873a8b9e2c7ccdd066f627451764a247dbb8728d
Reyaj Alam <mdreyajalal0007@gmail.com>
2021-08-10 11:48:33 +0530

    8th commit

--- practice/src/app/users.service.ts
+++ practice/src/app/users.service.ts
@@ -13,7 +13,7 @@ export class UsersService {
 
 getD(username:string, password:string){
   if (username =="reyaj" && password =="reyaj") {
-    localStorage.setItem('username', "9tab");
+    localStorage.setItem('username', "9TAB DIGITECH PRIVATE LIMITED-asdfkhafs863498b8bcr32874892738n428479284iuroweiurowrwriwurioueoiwiurniowcqriuoquioruwinqoriwurioucqutiemrtu0934u95784yt347cn3920,3ieo,2,i039x4859n758 5vy9n4h");
     return true;
   }else{
     return false
